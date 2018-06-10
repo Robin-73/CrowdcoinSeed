8020e4131b1712e9d4770bd23ce1527e713a4a15
Robin-73 <cedric@mariemarthe.com>
2018-06-10 18:22:05 +0200

    GetDefault port set Mainnet 12875

--- protocol.h
+++ protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 19414 : 9414;
+    return testnet ? 19414 : 12875;
 }
 
 //
